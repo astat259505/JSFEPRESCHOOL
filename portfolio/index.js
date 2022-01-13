312e20f835586f24aa00e09fa94c52409f6df4bf
Maxim Lizunkov <astat259505@gmail.com>
2022-01-13 22:32:10 +0300

    feat: add js code for adaptive nav

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,1 +1,22 @@
-console.log('Верстка валидная - ?\nВёрстка семантическая - 20\nВёрстка соответствует макету - 48\nТребования к css - 12\nИнтерактивность, реализуемая через css - 20\nИтого - 100');
+
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+
+const toggleMenu = () => {
+    nav.classList.toggle('open');
+    hamburger.classList.toggle('open');
+}
+
+hamburger.addEventListener('click', toggleMenu);
+
+
+const navLinks = document.querySelectorAll('.nav-link');
+
+const closeMenu = (event) => {
+    if (event.target.classList.contains('nav-link')) {
+    nav.classList.remove('open');
+    hamburger.classList.remove('open');
+    }
+}
+
+nav.addEventListener ('click', closeMenu);
