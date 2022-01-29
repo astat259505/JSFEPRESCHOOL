382da925fdfa7d20f5ad265df7882ac0baf36843
Maxim Lizunkov <astat259505@gmail.com>
2022-01-29 19:44:44 +0300

    feat: add playAudio and changePlaybackStatus functions

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,1 +1,29 @@
 let isPlay = false;
+
+const audio = document.querySelector('audio');
+
+const PlayAudio = () => {
+    if (!isPlay) {
+        audio.currentTime = 0;
+        audio.play();
+    } else {
+        audio.pause();
+    }
+}
+
+const playBtn = document.querySelector('.play-button');
+
+const changePlaybackStatus = () => {
+    playBtn.classList.toggle('pause');
+    if (playBtn.classList.contains('pause')) {
+        isPlay = false
+    } else {
+        isPlay = true
+    }
+}
+
+playBtn.addEventListener('click', () => {
+    changePlaybackStatus();
+    PlayAudio();
+}
+);
