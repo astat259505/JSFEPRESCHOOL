9dcbc2bbb71e1b61895347cdbc8eec1bd39199ae
Maxim Lizunkov <astat259505@gmail.com>
2022-02-16 21:31:58 +0300

    feat: add func that determine the winner combination

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,16 +1,57 @@
 let move = 0;
 const playingField = document.querySelector('.field');
+let result = ''
+const squares = document.querySelectorAll('.square')
+const actualResult = document.querySelector('.actual-result')
 
 const showSign = () => {
     playingField.addEventListener('click', (event) => {
-        if (move % 2 == 0) {
-            event.target.innerHTML = 'X'
-            move++
-        } else {
-            event.target.innerHTML = '0'
-            move++
+     move % 2 == 0 ? event.target.innerHTML = 'X' : event.target.innerHTML = '0'
+     move++
+     determineTheWinner()
+     showActualResult(move, result)
+            
+})
+
+
+}
+
+
+
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], 
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [0, 4, 8]
+]
+
+
+console.log(squares)
+console.log(winConditions[0][0])
+
+const determineTheWinner = () => {
+
+        for (i = 0; i < winConditions.length; i++){
+            if ((squares[winConditions[i][0]].innerHTML == 'X') && (squares[winConditions[i][1]].innerHTML == 'X') && (squares[winConditions[i][2]].innerHTML == 'X')) {
+                result = 'X win'
+            } if (squares[winConditions[i][0]].innerHTML == '0' && squares[winConditions[i][1]].innerHTML == '0' && squares[winConditions[i][2]].innerHTML == '0') {
+                result = '0 win'
+            }
         }
-    }) 
+    }
+
+const showActualResult = (move, result) => {
+    if (result != '') {
+        actualResult.innerHTML = `${result} ${move+1} turn`
+    } else {
+        actualResult.innerHTML = `${move+1} turn`
+    }
+
+
 }
 
 showSign()
