bca80bf93ea1f29665cb7dd6fec61eddac5cb542
Maxim Lizunkov <astat259505@gmail.com>
2022-02-17 21:42:54 +0300

    fix: turn off clicks after end game

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -4,19 +4,34 @@ let result = ''
 const squares = document.querySelectorAll('.square')
 const actualResult = document.querySelector('.actual-result')
 let gameStatistic = []
+const statistics = document.querySelectorAll('.statistics-item')
 
-const showSign = () => {
-    playingField.addEventListener('click', (event) => {
-     move % 2 == 0 ? event.target.innerHTML = 'X' : event.target.innerHTML = '0'
-     move++
+const gameProcess = () => {
+    squares.forEach(square => {
+        square.addEventListener('click', (event) => {
+        if (move % 2 == 0) {
+             event.target.innerHTML = 'X'
+             move++
+             event.target.style.pointerEvents = 'none'
+        } else {
+            event.target.innerHTML = '0'
+             move++
+             event.target.style.pointerEvents = 'none'
+        }
+
      determineTheWinner()
+     banClickAfterResult()
      showActualResult(move, result)
      recordGameResults()
+     showGameStatistic()
+     
             
 })
-
-
+    })
 }
+
+
+
 
 console.log(gameStatistic)
 
@@ -47,16 +62,18 @@ const determineTheWinner = () => {
 
         for (i = 0; i < winConditions.length; i++){
             if ((squares[winConditions[i][0]].innerHTML == 'X') && (squares[winConditions[i][1]].innerHTML == 'X') && (squares[winConditions[i][2]].innerHTML == 'X')) {
-                result = 'X win'
+                result = 'X won'
             } if (squares[winConditions[i][0]].innerHTML == '0' && squares[winConditions[i][1]].innerHTML == '0' && squares[winConditions[i][2]].innerHTML == '0') {
-                result = '0 win'
+                result = '0 won'
+            } if (move == 9) {
+                result = 'Draw'
             }
         }
     }
 
 const showActualResult = (move, result) => {
     if (result != '') {
-        actualResult.innerHTML = `${result} ${move+1} turn`
+        actualResult.innerHTML = `${result} ${move} turn`
     } else {
         actualResult.innerHTML = `${move+1} turn`
     }
@@ -74,9 +91,24 @@ window.addEventListener('beforeunload', setLocalStorage)
 const getLocalStorage = () => {
     
     gameStatistic = JSON.parse(localStorage.getItem('gameStatistic'))
+    showGameStatistic()
     
 }
 
 window.addEventListener('load', getLocalStorage)
 
-showSign()
+gameProcess()
+
+const showGameStatistic = () => {
+  statistics.forEach((item, index) => {
+      item.innerHTML = `${gameStatistic[index]}`
+  })
+}
+
+const banClickAfterResult = () => {
+    squares.forEach(square => {
+    if (result != '') {
+        square.style.pointerEvents = 'none'
+    }
+})
+}
